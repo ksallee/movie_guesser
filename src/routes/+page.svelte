a6a985a8666e360075c4f0a8dd8dc4263c85faa6
Kevin Sallee <kevin.sallee@gpltech.com>
2025-02-06 09:17:37 -0600

    footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,11 @@
 <div class="game-container">
   <h1 class="game-title">Guess The Movie</h1>
 	<MovieGuesser />
-	<VisitorCounter />
+	<div class="footer">
+		<VisitorCounter />
+		<p class="footer-text">Made with ❤️ by <a href="mailto:kevin.sallee@gmail.com">Kevin Sallée </a></p>
+	</div>
+
 
 </div>
 
@@ -27,6 +31,9 @@
 		padding: var(--spacing-md);
 		background: var(--color-bg);
 		color: var(--color-text);
+		@media (max-width: 768px) {
+			padding: var(--spacing-sm);
+		}
 	}
 
 	.game-title {
@@ -35,5 +42,22 @@
 		font-size: var(--font-size-2xl);
 		font-weight: var(--font-weight-normal);
 		margin-top: var(--spacing-lg);
+	}
+	.footer{
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		width: 100%;
+		justify-content: space-between;
+		gap: var(--spacing-lg);
+	}
+	.footer-text{
+		font-size: var(--font-size-xs);
+		color: var(--color-neutral-500);
+		text-align: right;
+	}
+	a{
+		color: var(--color-primary);
+		white-space: nowrap;
 	}
 </style>
