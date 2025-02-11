827329548c504c87277927179a55561ec02c7612
Kevin Sallee <kevin.sallee@gpltech.com>
2025-02-11 13:10:06 -0600

    removed url

--- src/routes/quizzes/[id]/share/[title]/[score]/[accuracy]/+page.svelte
+++ src/routes/quizzes/[id]/share/[title]/[score]/[accuracy]/+page.svelte
@@ -1,8 +1,7 @@
 <script>
     import SEO from '$lib/components/SEO.svelte';
     import { page } from '$app/state';
-
-    const quizId = page.params.id;
+		
     const score = page.params.score;
 		const accuracy = page.params.accuracy;
 		const title = page.params.title;
@@ -13,6 +12,5 @@
     title="I scored {score} points in Guess The Movie!"
     description="I completed '{title}' with {accuracy}% accuracy! Can you beat my score?"
     image="/images/quizzes_preview.jpg"
-    url="https://guessthemoviegame.netlify.app/share/{quizId}?score={score}&accuracy={accuracy}&title={title}"
 />
 <h1>Nothing to see here</h1>
